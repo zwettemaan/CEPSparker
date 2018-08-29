9bec111e10f8532e1effd8259c360e9c89e86962
Kris Coppieters <kris@rorohiko.com>
2018-08-30 09:27:56 +1200

    Debug mode is not CEP-version specific (e.g. to debug a CEP 4.2 extension under CEP 6, the debug mode for CEP 6 needs to be set).

--- Mac/clearPlayerDebugMode.command
+++ Mac/clearPlayerDebugMode.command
@@ -2,42 +2,18 @@
 # Disable debugging
 #
 
-export scriptDir=`dirname "$0"`
-cd $scriptDir
-export scriptDir=`pwd`
-export projectHomeDir=`dirname "$scriptDir"`
+defaults write com.adobe.CSXS.4.plist PlayerDebugMode 0
+defaults write com.adobe.CSXS.4.plist LogLevel 1
+defaults write com.adobe.CSXS.5.plist PlayerDebugMode 0
+defaults write com.adobe.CSXS.5.plist LogLevel 1
+defaults write com.adobe.CSXS.6.plist PlayerDebugMode 0
+defaults write com.adobe.CSXS.6.plist LogLevel 1
+defaults write com.adobe.CSXS.7.plist PlayerDebugMode 0
+defaults write com.adobe.CSXS.7.plist LogLevel 1
+defaults write com.adobe.CSXS.8.plist PlayerDebugMode 0
+defaults write com.adobe.CSXS.8.plist LogLevel 1
 
-cd "$projectHomeDir"
-
-if [ -f  "$projectHomeDir/BuildSettings/CEPVersion.txt" ]; then
-
-    export CEP_VERSION=`head -n 1 "$projectHomeDir/BuildSettings/CEPVersion.txt"`
-
-    if [ "$CEP_VERSION" != "" ]; then
-    
-        if [ "$CEP_VERSION" == "4.2" ]; then
-            export PLIST_FILE=com.adobe.CSXS.4.plist
-        elif [ "$CEP_VERSION" == "5.2" ]; then
-            export PLIST_FILE=com.adobe.CSXS.5.plist
-        elif [ "$CEP_VERSION" == "6.1" ]; then
-            export PLIST_FILE=com.adobe.CSXS.6.plist
-        elif [ "$CEP_VERSION" == "7.0" ]; then
-            export PLIST_FILE=com.adobe.CSXS.7.plist
-        elif [ "$CEP_VERSION" == "8.0" ]; then
-            export PLIST_FILE=com.adobe.CSXS.8.plist
-        else
-            echo "Unexpected CEP_VERSION"
-        fi
-
-        if [ "$PLIST_FILE" != "" ]; then
-
-            defaults write $PLIST_FILE PlayerDebugMode 0
-            defaults write $PLIST_FILE LogLevel 1
-
-            #
-            # Force Mac OS to re-read the PLIST file
-            #
-            killall -u `whoami` cfprefsd
-        fi
-    fi
-fi
+#
+# Force Mac OS to re-read the PLIST file
+#
+killall -u `whoami` cfprefsd
--- Windows/setPlayerDebugMode.bat
+++ Windows/setPlayerDebugMode.bat
@@ -3,60 +3,13 @@ REM
 REM Enable debugging
 REM
 
-SET scriptDir=%~dp0
-PUSHD "%scriptDir%.."
-SET projectHomeDir=%cd%\
-POPD
-
-PUSHD "%projectHomeDir%"
-
-IF NOT EXIST BuildSettings\CEPVersion.txt (
-
-    ECHO Error: This CEPSparker folder has not been initialized. Make
-    ECHO sure to run the CEPSparkerConfig.exe command first. Aborting.
-    POPD
-    EXIT /b
-)
-
-SET /p CEP_VERSION=< BuildSettings\CEPVersion.txt
-
-IF "%CEP_VERSION%" == "" (
-
-    ECHO Error: Cannot determine CEPVersion to be targeted. Aborting.
-    POPD
-    EXIT /b
-)
-
-SET recognized="NO"
-
-IF "%CEP_VERSION%" == "4.2" (
-    REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.4 /v PlayerDebugMode /t REG_SZ /d 1 /f > NUL
-    REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.4 /v LogLevel /t REG_SZ /d 5 /f > NUL
-    SET recognized="YES"
-)
-IF "%CEP_VERSION%" == "5.2" (
-    REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.5 /v PlayerDebugMode /t REG_SZ /d 1 /f > NUL
-    REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.5 /v LogLevel /t REG_SZ /d 5 /f > NUL
-    SET recognized="YES"
-)
-IF "%CEP_VERSION%" == "6.1" (
-    REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.6 /v PlayerDebugMode /t REG_SZ /d 1 /f > NUL
-    REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.6 /v LogLevel /t REG_SZ /d 5 /f > NUL
-    SET recognized="YES"
-)
-IF "%CEP_VERSION%" == "7.0" (
-    REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.7 /v PlayerDebugMode /t REG_SZ /d 1 /f > NUL
-    REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.7 /v LogLevel /t REG_SZ /d 5 /f > NUL
-    SET recognized="YES"
-)
-IF "%CEP_VERSION%" == "8.0" (
-    REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.8 /v PlayerDebugMode /t REG_SZ /d 1 /f > NUL
-    REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.8 /v LogLevel /t REG_SZ /d 5 /f > NUL
-    SET recognized="YES"
-)
-
-IF %recognized% == "NO" (
-    ECHO "Unexpected CEP_VERSION"
-)
-
-POPD
+REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.4 /v PlayerDebugMode /t REG_SZ /d 1 /f > NUL
+REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.4 /v LogLevel /t REG_SZ /d 5 /f > NUL
+REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.5 /v PlayerDebugMode /t REG_SZ /d 1 /f > NUL
+REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.5 /v LogLevel /t REG_SZ /d 5 /f > NUL
+REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.6 /v PlayerDebugMode /t REG_SZ /d 1 /f > NUL
+REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.6 /v LogLevel /t REG_SZ /d 5 /f > NUL
+REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.7 /v PlayerDebugMode /t REG_SZ /d 1 /f > NUL
+REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.7 /v LogLevel /t REG_SZ /d 5 /f > NUL
+REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.8 /v PlayerDebugMode /t REG_SZ /d 1 /f > NUL
+REG add HKEY_CURRENT_USER\Software\Adobe\CSXS.8 /v LogLevel /t REG_SZ /d 5 /f > NUL
