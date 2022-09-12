d1d56286d6b9309e505b24e4ec03082304e63358
Kris Coppieters <kris@rorohiko.com>
2022-09-12 16:27:13 +1200

    Incremental improvements

--- Templates/js/themeManager.js
+++ Templates/js/themeManager.js
@@ -44,12 +44,6 @@ var themeManager = (function () {
     // console.log(appSkinInfo)
     var panelBgColor = appSkinInfo.panelBackgroundColor.color;
     var bgdColor = toHex(panelBgColor);
-    // KC20220912 Actual BG colors measured on my Mac, dark to light
-    // These seem to be different than what appSkinInfo has
-    // 323232
-    // 535353
-    // b9b9b9
-    // F0F0F0
     var fontColor = "F0F0F0";
     if (panelBgColor.red > 122) {
       fontColor = "000000";
--- Templates/jsx/manuallyClearGlobalsIDSN_ES_FromDebugger.jsx
+++ Templates/jsx/manuallyClearGlobalsIDSN_ES_FromDebugger.jsx
@@ -1,3 +1,4 @@
+//@targetengine $$EXTENSION_ID$$_Engine_Id
 $$SHORTCODE$$ = undefined;
 JSInterface = undefined;
 JSON = undefined;
