1fad68513f20358e0a6d5a60c5666a07b83b3caf
Kris Coppieters <kris@rorohiko.com>
2024-04-24 18:13:43 +1200

    Windows packaging

---
+++ Windows/postPackage.bat
@@ -0,0 +1,78 @@
+@ECHO OFF
+REM
+REM Called by PluginManager by way of CRDT_manifest.json. 
+REM
+
+IF "%SPRK_COMMANDS_DIR%" == "" (
+    SET SPRK_COMMANDS_DIR=%~dp0
+)
+
+SET SOURCE_PACKAGE_FOLDER=%1
+SET TARGET_PACKAGE_FOLDER=%2
+SET FILE_PATH_PLUGIN_INSTALLER_RESOURCES=%3
+SET FILE_PATH_CODE_SIGN_CERTIFICATE=%4
+SET CODE_SIGN_CERTIFICATE_PASSWORD=%5
+SET URL_TIME_STAMP_SERVER="6
+SET INJECT_DEBUG_FILE=%7
+
+SET PACKAGE_NAME=%~nx2
+PUSHD "%TARGET_PACKAGE_FOLDER%.."
+SET PACKAGE_PARENT_DIR=%cd%\
+POPD
+SET UCF_TEMP_DIR=%TEMP%\CRDT_Temp_UCF\
+SET JRE=windows_x86_64_jre.zip
+
+PUSHD "%SPRK_COMMANDS_DIR%.."
+SET PROJECT_ROOT_DIR=%cd%\
+POPD
+
+IF "%PROCESSOR_ARCHITECTURE%" == "ARM64" GOTO UNSUPPORTED_ARM
+
+CALL "%SPRK_COMMANDS_DIR%setTarget.bat"
+CALL "%SPRK_COMMANDS_DIR%adjustVersionInManifest.bat" NESTED
+CALL "%PROJECT_ROOT_DIR%BuildSettings\buildSettings.bat"
+
+POWERSHELL -command "Expand-Archive -Force '%FILE_PATH_PLUGIN_INSTALLER_RESOURCES%\%JRE%' '%UCF_TEMP_DIR%'"
+
+SET JAVA_HOME=%UCF_TEMP_DIR%jre\Contents\Home
+PATH=%UCF_TEMP_DIR%jre\Contents\Home\bin;%PATH%
+
+cd "%PACKAGE_PARENT_DIR%"
+
+REM JSON files get rewritten by ucf.jar as they get included into the ZXP - move the CRDT_manifest.json away
+REM so it is not included into the code-signing.
+REM We will restore it beside the ZXP file
+
+MOVE %PACKAGE_NAME%\CRDT_manifest.json "%UCF_TEMP_DIR%"
+
+IF  "%INJECT_DEBUG_FILE%" == "1" (
+    IF EXIST %SOURCE_PACKAGE_FOLDER%\debug (
+        java -jar "%FILE_PATH_PLUGIN_INSTALLER_RESOURCES%\ucf.jar" -package -storetype PKCS12 -keystore "%FILE_PATH_CODE_SIGN_CERTIFICATE%" -storepass "%CODE_SIGN_CERTIFICATE_PASSWORD%" -tsa "%URL_TIME_STAMP_SERVER%" "%PACKAGE_NAME%.zxp" -C "%TARGET_PACKAGE_FOLDER%" . -e "%SOURCE_PACKAGE_FOLDER%\debug" .debug
+    ) ELSE (
+        java -jar "%FILE_PATH_PLUGIN_INSTALLER_RESOURCES%\ucf.jar" -package -storetype PKCS12 -keystore "%FILE_PATH_CODE_SIGN_CERTIFICATE%" -storepass "%CODE_SIGN_CERTIFICATE_PASSWORD%" -tsa "%URL_TIME_STAMP_SERVER%" "%PACKAGE_NAME%.zxp" -C "%TARGET_PACKAGE_FOLDER%" .
+    )    
+)
+
+MOVE "%PACKAGE_NAME%" "%PACKAGE_NAME%.precursor"
+
+MD "%PACKAGE_NAME%"
+
+MOVE "%PACKAGE_NAME%.zxp" "%PACKAGE_NAME%"
+
+MOVE "%UCF_TEMP_DIR%\CRDT_manifest.json" "%PACKAGE_NAME%"
+
+RD /s /q "%PACKAGE_NAME%.precursor"
+RD /s /q "%UCF_TEMP_DIR%.precursor"
+
+IF NOT EXIST "%SOURCE_PACKAGE_FOLDER%\build" MKDIR "%SOURCE_PACKAGE_FOLDER%\build"
+
+COPY "%PACKAGE_NAME%\%PACKAGE_NAME%.zxp" "%SOURCE_PACKAGE_FOLDER%""
+
+GOTO DONE
+
+:UNSUPPORTED_ARM
+ECHO Windows on ARM is currently unsupported
+GOTO DONE
+
+
+:DONE
